cb8ca2c6924ac2b19cad355ff397fc86b345e65a
nour ganouni <nour.ganouni.11@gmail.com>
2025-09-27 20:23:12 +0100

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../styles/Footer.css';
+function Footer() {
+  return (
+    <div className="footer">
+        <div className="socialMedia">
+            <InstagramIcon/> <TwitterIcon/> <FacebookIcon/> <LinkedInIcon/>
+        </div>
+        <p>Copyright &copy; 2023 nanou's resto</p>
+        
+      
+    </div>
+  )
+}
+
+export default Footer
